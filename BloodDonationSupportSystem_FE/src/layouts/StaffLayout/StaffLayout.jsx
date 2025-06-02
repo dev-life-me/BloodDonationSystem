f8daceb8b275c92ba878e183e890beb9b2847569
Le Hong Duc <pduc2200@gmail.com>
2025-06-02 13:37:24 +0700

    them StaffPage voi StaffLayout

---
+++ BloodDonationSupportSystem_FE/src/layouts/StaffLayout/StaffLayout.jsx
@@ -0,0 +1,34 @@
+import { Box } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import StaffSidebar from './StaffSidebar';
+
+const StaffLayout = () => {
+  return (
+    <Box sx={{ display: 'flex', minHeight: '100vh' }}>
+      {/* Sidebar chiếm 20% */}
+      <Box
+        sx={{
+          flex: 2,
+          bgcolor: 'background.paper',
+          borderRight: '1px solid #ddd',
+        }}
+      >
+        <StaffSidebar/>
+      </Box>
+
+      {/* Nội dung chiếm 80% */}
+      <Box
+        sx={{
+          flex: 8,
+          p: 4,
+          overflowX: 'hidden',
+          bgcolor: 'background.default',
+        }}
+      >
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default StaffLayout;
